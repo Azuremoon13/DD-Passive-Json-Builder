a7426cdf7962789fb05cd4648d55dc6d3505a812
Azuremoon13 <joe.bolt@icloud.com>
2025-03-20 19:09:54 +0000

    updated icon/remove theme

--- JsonBuilder.py
+++ JsonBuilder.py
@@ -314,7 +314,6 @@ class JSONBuilder:
 
 if __name__ == "__main__":
 	ctk.set_appearance_mode("dark")
-	ctk.set_default_color_theme("blue")
 	root = ctk.CTk()
 	app = JSONBuilder(root)
 	root.mainloop()
